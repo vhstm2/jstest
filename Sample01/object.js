395f93d5801e20c590ce89fc9150a009e8f99038
hyun19899 <hyun19899@naver.com>
2018-11-20 14:59:09 +0900

    object.js

---
+++ Sample01/object.js
@@ -0,0 +1,24 @@
+
+//객체 생성
+//var obj  = new object();
+//변수가 메모리 주소에 있는 메모리 공간을 할당해 obj가 사용할수있게 한다.
+
+
+var obj = {};
+obj.name ="Sanghyun";
+console.log(obj.name);
+
+var obj2 = {};
+obj2["name"] = "홍길동";
+console.log(obj2.name);
+
+var parson =
+{
+    name :"홍길동",
+    age : 33,
+    address : 
+    {
+        zipcode : 14500,
+        city : "서울"
+    }
+}
